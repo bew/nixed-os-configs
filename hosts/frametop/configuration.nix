85b0d3e26d5e31da1498df41bae60ab1747d99f0
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2022-12-26 00:46:02 +0100

    Fix locale for unicode chars input in zsh/.. !

--- hosts/frametop/configuration.nix
+++ hosts/frametop/configuration.nix
@@ -41,13 +41,15 @@
   i18n.extraLocaleSettings = let
     nativeLang = "fr_FR.UTF-8";
   in {
-    LC_ALL = "C";
     LC_MEASUREMENT = nativeLang;
     LC_MONETARY = nativeLang;
     LC_NUMERIC = nativeLang;
     LC_PAPER = nativeLang;
     LC_TELEPHONE = nativeLang;
     LC_TIME = nativeLang;
+    # NOTE: `LC_ALL` should only be set for troubleshooting, it overrides every
+    # local settings, can be set to C.UTF-8 for simple debugs (while still
+    # supporting unicode chars).
   };
 
   console = {
