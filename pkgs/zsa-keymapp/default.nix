f0df91d238b42129841a2b13c4d40b14324962b0
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2024-03-07 20:23:21 +0100

    keymapp: Update @2024-03-08 (remove binary, nix is broken with git-lfs)
    Using git-lfs, when the flake copies the repo to the store (for purity)
    the 'virtual file' stored in git is copied (with oid/size info of the
    object in LFS) instead of the actual (large) file :/
    ref: https://github.com/NixOS/nix/pull/10153
    
    I think it was working before because the file was in git temporarily at
    some point, then I moved it to LFS, but after the system was built..
    (or something like that 🤷)

--- pkgs/zsa-keymapp/default.nix
+++ pkgs/zsa-keymapp/default.nix
@@ -1,24 +1,27 @@
 {
   buildFHSUserEnv,
   writeShellScript,
+  fetchzip,
 }:
 
 # Copied/Adapted from: https://github.com/pope/personal/commit/5cf0d43c7ffec572b46fcd96f08c5e9748ead9d4
 let
 
-  # @2023-11 ZSA doesn't seem to have published Keymapp' sources,
+  # @2023-11 ZSA will not publish Keymapp' sources,
   # and only give a download link to the LATEST version of the built software.
-  #
-  # In the meantime, download the latest version from:
-  #   https://oryx.nyc3.cdn.digitaloceanspaces.com/keymapp/keymapp-latest.tar.gz
-  # And put the binary locally in the repo (through Git LFS).
-  keymapp-binary = ./keymapp;
+  # NOTE: fetchzip is not limited to zip files and supports many archives formats
+  keymapp-binary = fetchzip {
+    url = "https://oryx.nyc3.cdn.digitaloceanspaces.com/keymapp/keymapp-latest.tar.gz";
+    # WARNING: The URL is impure, it does not lock to a version, so the hash can easily be outdated
+    #   (and I'd be aware of that only if I do a re-install without my current store)
+    hash = "sha256-+tunfmnjFsqhkmzK3V6efcJ9u3qapx41JUzH/z4dUis";
+    # archive contains binary + an icon file, don't try to extract a single file
+    stripRoot = false;
+  };
 
 in buildFHSUserEnv {
   name = "keymapp";
-  runScript = writeShellScript "keymapp-wrapper.sh" ''
-    exec ${keymapp-binary} "$@"
-  '';
+  runScript = "${keymapp-binary}/keymapp";
   targetPkgs = pkgs: [
     pkgs.gdk-pixbuf
     pkgs.glib
