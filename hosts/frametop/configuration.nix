78f6da053016ceaefa3e2a7aa1cd2cd2e3acc381
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2024-03-17 17:59:34 +0100

    feat(network): Default open port 1234 I usually use for short-term tests

--- hosts/frametop/configuration.nix
+++ hosts/frametop/configuration.nix
@@ -232,10 +232,10 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [
+    1234 # usually used for SHORT-TERM tests, should be open if used!
+  ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
