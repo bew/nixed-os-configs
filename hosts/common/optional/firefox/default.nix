494561698b5e08999ff2bc8323799c8caaeb7b08
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2024-11-23 14:46:56 +0100

    firefox: Use XInput2 on X11 to support modern touchpad gestures

--- hosts/common/optional/firefox/default.nix
+++ hosts/common/optional/firefox/default.nix
@@ -48,6 +48,11 @@
     "browser.newtabpage.activity-stream.default.sites" = ""; # Clear default TopSites
   };
 
+  # Use XInput2 on X11 to support modern _precise_ touchpad gestures
+  # (like 2-fingers swipe for backward/forward history & pinch-to-zoom)
+  # REF: https://bugzilla.mozilla.org/show_bug.cgi?id=1539730
+  environment.sessionVariables.MOZ_USE_XINPUT2 = "1";
+
   # doc: https://mozilla.github.io/policy-templates/
   programs.firefox.policies = {
     # Remove the “Set As Desktop Background…” menuitem when right clicking on an image.
