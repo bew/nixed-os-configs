ace80996944a5443e5ef70602248b6a1da2bc11a
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-07-05 03:07:39 +0200

    misc

--- profiles/os-nixos/workstation/system-tools.nix
+++ profiles/os-nixos/workstation/system-tools.nix
@@ -2,9 +2,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-    gparted
-    ntfs3g
-    kdePackages.filelight
+    gparted # disk partitioning tool
+    kdePackages.filelight # disk usage stats
+    ntfs3g # ntfs driver tools
 
     lshw # Show hardware info
     pciutils # Show pci hardware info (e.g. lspci)
