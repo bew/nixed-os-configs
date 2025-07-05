d09fe60fbfae8d83564b59a87cee53a51d21494b
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-07-05 21:59:11 +0200

    Copy module from 24.11 to have netdata OSS with Web UI (not in v2 😬)

--- hosts/frametop/default.nix
+++ hosts/frametop/default.nix
@@ -5,10 +5,15 @@ let
   # expose inputs
   system-nixpkgs = loadFlake inputs.system-nixpkgs;
   nixos-hardware = loadFlake inputs.nixos-hardware;
+  # note: netdata v2+ is now cloud-only, no local web ui available anymore
+  #   so we force the use of the last netdata v1 we have in 24.11
+  netdata-nixpkgs = loadFlake inputs.netdata-nixpkgs;
+
+  system = "x86_64-linux";
 in
 
 system-nixpkgs.lib.nixosSystem {
-  system = "x86_64-linux";
+  inherit system;
   modules = [
     {
       imports = [ nixos-hardware.nixosModules.framework-12th-gen-intel ];
@@ -19,6 +24,19 @@ system-nixpkgs.lib.nixosSystem {
     {
       nix.registry.syspkgs.flake = system-nixpkgs;
     }
+
+    ({ config, lib, ... }: {
+      imports = [ ../../modules/os-nixos/netdata-oss.nix ];
+      services.netdata-oss.package = netdata-nixpkgs.legacyPackages.${system}.netdata;
+      assertions = lib.mkIf config.services.netdata-oss.enable [
+        {
+          assertion = let
+            netdataVersion = lib.getVersion config.services.netdata-oss.package;
+          in lib.versionOlder netdataVersion "2.0";
+          message = "Netdata must be before 2.0 to be usable locally with the web-UI, v2+ is cloud-only";
+        }
+      ];
+    })
 
     ./system.nix
     ./configuration.nix
--- profiles/os-nixos/workstation/monitoring.nix
+++ profiles/os-nixos/workstation/monitoring.nix
@@ -1,8 +1,13 @@
 {
   # Let's monitor my PC!
-  services.netdata.enable = true;
+
+  # NOTE: using my own `netdata-oss` service module to use netdata v1
+  #   (v1 _still_ has the local Web UI, but v2 is cloud-only)
+
+  services.netdata-oss.enable = true;
   # NOTE: easily check current config at <http://localhost:19999/netdata.conf>
-  services.netdata.config = {
+
+  services.netdata-oss.config = {
     # Enable more metrics around power supply
     "plugin:proc:/sys/class/power_supply" = {
       "battery capacity" = "yes"; # the default
